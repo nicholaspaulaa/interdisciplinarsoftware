4cdca68aee8dc94af8bdce17ce6518131a09712a
Vinicius Domingues <vinilecrofi@gmail.com>
2026-06-22 12:44:06 -0300

    feat(model): implementa equacao para calcular Ptx (Transmissao)

--- src/model/LinkBudgetCalculadora.java
+++ src/model/LinkBudgetCalculadora.java
@@ -4,8 +4,10 @@ public class LinkBudgetCalculadora {
     private double calcularPerdaTotal(RedePassiva rede) {
         return (rede.getAtenuacaoFibra() * rede.getDistanciaKm()) + rede.getPerdasPontuais();
     }
-
     private double calcularPrx(Equipamento eq, RedePassiva rede) {
         return eq.getPtx() - calcularPerdaTotal(rede) - eq.getMargem();
+    }
+    private double calcularPtx(Equipamento eq, RedePassiva rede) {
+        return eq.getPrx() + calcularPerdaTotal(rede) + eq.getMargem();
     }
 }
