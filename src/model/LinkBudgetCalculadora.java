d11807ed5bbdae87a7512e565b42f4251b8e3456
Vinicius Domingues <vinilecrofi@gmail.com>
2026-06-22 12:43:46 -0300

    feat(model): implementa equacao para calcular Prx (Sensibilidade)

--- src/model/LinkBudgetCalculadora.java
+++ src/model/LinkBudgetCalculadora.java
@@ -4,4 +4,8 @@ public class LinkBudgetCalculadora {
     private double calcularPerdaTotal(RedePassiva rede) {
         return (rede.getAtenuacaoFibra() * rede.getDistanciaKm()) + rede.getPerdasPontuais();
     }
+
+    private double calcularPrx(Equipamento eq, RedePassiva rede) {
+        return eq.getPtx() - calcularPerdaTotal(rede) - eq.getMargem();
+    }
 }
