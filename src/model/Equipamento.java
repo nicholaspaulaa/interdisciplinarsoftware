b204258f49051bd16ed0fc9bd7e2219a98c9f406
nicholaspaulaa <nicholas.emanuell1@gmail.com>
2026-06-22 02:19:57 -0300

    novas classes, model, pacote

---
+++ src/model/Equipamento.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Equipamento {
+    private Double ptx;
+    private Double prx;
+    private Double margem;
+
+    public Equipamento(Double ptx, Double prx, Double margem) {
+        this.ptx = ptx;
+        this.prx = prx;
+        this.margem = margem;
+    }
+}
