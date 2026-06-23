919b927f65c6ab6d41b1f9bfe11f0f3a1ced828c
erica <ericasguerzoni@gmail.com>
2026-06-22 22:22:47 -0300

    feat(controller): implementa try-catch com validacao e calculo final

--- src/controller/CalculadoraController.java
+++ src/controller/CalculadoraController.java
@@ -3,18 +3,21 @@ package controller;
 import model.*;
 
 public class CalculadoraController {
-    private LinkBudgetCalculadora calculadora;
-    private ValidadorG984 validador;
-
-    public CalculadoraController() {
-        this.calculadora = new LinkBudgetCalculadora();
-        this.validador = new ValidadorG984();
-    }
+    private LinkBudgetCalculadora calc = new LinkBudgetCalculadora();
+    private ValidadorG984 validador = new ValidadorG984();
 
     public String processarCalculo(Double ptx, Double prx, Double margem, Double dist,
-                                   Double atenuacao, Double splitters, Double conect, Double fusoes) {
-        Equipamento eq = new Equipamento(ptx, prx, margem);
-        RedePassiva rede = new RedePassiva(dist, atenuacao, splitters, conect, fusoes);
-        return "Processando...";
+                                   Double aten, Double splitters, Double conect, Double fusoes) {
+        try {
+            Equipamento eq = new Equipamento(ptx, prx, margem);
+            RedePassiva rede = new RedePassiva(dist, aten, splitters, conect, fusoes);
+
+            validador.validarParametrosFisicos(rede);
+            return calc.calcularVariavelFaltante(eq, rede);
+        } catch (ValidacaoException e) {
+            return e.getMessage();
+        } catch (Exception e) {
+            return "ERRO GRAVE: Falha de processamento.";
+        }
     }
 }
